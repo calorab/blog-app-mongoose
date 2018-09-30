f88ac45674cb6df8955b72e01ec1d3d59a6d987d
Caleb Gammon <calebwgammon@gmail.com>
2018-09-30 17:00:28 -0400

    added server config models

--- models.js
+++ models.js
@@ -4,30 +4,40 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const blogPostSchema = mongoose.Schema({
-  author: {
-    firstName: String,
-    lastName: String
-  },
-  title: {type: String, required: true},
-  content: {type: String},
-  created: {type: Date, default: Date.now}
+    author: {
+        firstName: String,
+        lastName: String
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-
-blogPostSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
+blogPostSchema.virtual('authorName').get(function () {
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-blogPostSchema.methods.serialize = function() {
-  return {
-    id: this._id,
-    author: this.authorName,
-    content: this.content,
-    title: this.title,
-    created: this.created
-  };
+blogPostSchema.methods.serialize = function () {
+    return {
+        id: this._id,
+        author: this.authorName,
+        content: this.content,
+        title: this.title,
+        created: this.created
+    };
 };
 
-const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+const BlogPost = Mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = {BlogPost};
+module.exports = {
+    BlogPost
+};
